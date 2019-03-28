39c560a5c0d8f64cc2d9052f863db4abc85a5249
Glitch (honeysuckle-turtle) <none>
2019-03-28 05:20:44 +0000

    ❣️⛷ Updated with Glitch

--- server.js
+++ server.js
@@ -1,219 +1,133 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
+'use strict';
 
-const cors = require('cors')
+var express = require('express');
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var mongodb = require('mongodb').MongoClient;
+mongoose.connect(process.env.MONGO_URL, {autoIndex: false});
 
-const mongoose = require('mongoose')
-mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track' )
+var cors = require('cors');
 
-app.use(cors())
+var app = express();
 
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
+// Basic Configuration 
+var port = process.env.PORT || 3000;
 
-
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
-});
-
+/** this project needs a db !! **/ 
+// mongoose.connect(process.env.MONGOLAB_URI);
 var Schema = mongoose.Schema;
-var exerciseSchema = new Schema({
-  userId : String,
-  description: String,
-  duration: Number, 
-  date: Date,
+var UrlSchema  = new Schema({
+  original_url: String, 
+  short_url: String
 });
-var userSchema = new Schema({
-  name: String,
-  id: String,
-});
-var User = mongoose.model('User',userSchema);
-var Exercise = mongoose.model('Exercise',exerciseSchema);
+var Url = mongoose.model('Url',UrlSchema);
 
-//hello api
+var url = new Url({
+  original_url: "https://www.google.com",
+  short_url: "1"
+});
+
+app.use(cors());
+
+/** this project needs to parse POST bodies **/
+// you should mount the body-parser here
+
+app.use(bodyParser.urlencoded({extended: 'false'}));
+app.use(bodyParser.json());
+
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function(req, res){
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+  
+// your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  var exercise = new Exercise({
-    userId: 'asdfv',
-    description: 'person exercising',
-    duration: 123,
-    date:Date.now(),
-  });
-  var user = new User({
-    name: 'abhi',
-    log:[exercise],
-  });
-  user.save((err,data)=>{
-    res.json(data);  
-  });
+  res.json({greeting: 'hello API'});
 });
 
-//create new user
-app.post("/api/exercise/new-user",(req, res)=>{
-  var userName = req.body['username'];
-  var user = new User({
-    name: userName,
-    id: generateID(),
-  });
-  user.save((err,data)=>{
-    console.log(data,err);
-    if(err && err.code == '11000'){
-        res.send("username already taken");
-    }
-    else{
-      res.json({username: data.name, _id: data.id});  
-    }
-  });
-});
-
-
-//create new exercise
-app.post("/api/exercise/add",(req, res)=>{
-  var userId = req.body['userId'];
-  var description = req.body['description'];
-  var duration, date;
-  duration = parseInt(req.body['duration'],10);
-  date = Date.parse(req.body['date']);
-  const dateError = 'Cast to Date failed for value "'+req.body['date']+'" at path "date"';
-  const durationError = 'Cast to Number failed for value "'+req.body['duration']+'" at path "duration"';
-  console.log(userId + description + duration + date);
-  if(!date){
-    res.send(dateError);
-  }
-  else if(!duration){
-    res.send(durationError);
-  }
-  else{
-    var newExercise = new Exercise({
-      userId: userId, 
-      description: description,
-      duration: duration,
-      date: date
-    });
-    User.findOne({id: userId},(err, user)=>{
-      console.log(err,user);
-      if(user == null){
-        res.send("unknown _id");
-      }
-      else{
-        newExercise.save((err,data)=>{
-          var options = {  weekday: 'short', year: 'numeric', month: 'short', day: 'numeric'};
-          var prnDt = new Date(date).toLocaleTimeString('en-us', options);
-          res.json({
-            username: user.name,
-            description: description,
-            duration: duration,
-            _id: userId,
-            date: prnDt,
-          });
-        });
-      }
-    });
-  }
-});
-
-const generateID = ()=>{
-return Math.random().toString(36).slice(-9);  
-};
-const parseDate = (str) => {
-    if(!/^(\d){8}$/.test(str)) throw('dateError');
-    var y = str.substr(0,4),
-        m = str.substr(4,2) - 1,
-        d = str.substr(6,2);
-    return new Date(y,m,d);
+var isUrlValid = function isUrlValid(userInput) {
+    var res = userInput.match(/^(http(s)?:\/\/www\.)[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+    if(res == null)
+        return false;
+    else
+        return true;
 }
 
+var findByShortUrl = function(shortUrl, done){
+Url.findOne({short_url: shortUrl},(err,data)=>{
+  done(data);
+  });
+}
 
-//fetch exercise
-app.get("/api/exercise/log",(req,res)=>{
-  console.log("get call recieved.");
-  var userId = req.query['userId'];
-  var from = req.query['from'];
-  var to = req.query['to'];
-  var limit = parseInt(req.query['limit']);
-  console.log(userId+ " "+ from + " " + to + " "+  limit);
-  var dateQuery = {};
-  var query = {};
-  
-  if(userId){
-    query['userId'] = userId; 
+var findByFullUrl = function(fullUrl, done){
+Url.findOne({original_url: fullUrl},(err,data)=>{
+  done(data);
+});
+}
+
+app.get("/api/shorturl/:url",(req,res)=>{
+  var errorObject = {
+    "error":"No short url found for given input"
+  }; 
+  Url.find({short_url: req.params['url']},(err,data)=>{
+    if(data == null){
+      res.json(errorObject);
+    }
+    res.json(data);
+  });
+});
+
+var findLastShortUrl = function(){
+  var short_url= Url.findOne({})
+    .sort({short_url: -1})
+    .limit(1)
+    .select('short_url');
+  return short_url;
+};
+
+app.post("/api/shorturl/new",(req,res)=>{
+  var url = req.body['url'];
+  var errorObj = {"error":"invalid URL"};
+  if(isUrlValid(url)){
+    findByFullUrl(url,(result)=>{
+      if(result == null){
+        findLastShortUrl().then((result)=>{
+          var nextShortUrl = parseInt(result.short_url) + 1;
+          var newUrl = new Url({
+            original_url: url,
+            short_url: nextShortUrl
+          }); 
+        newUrl.save()
+          res.send(
+            {"original_url":newUrl.original_url,
+             "short_url":newUrl.short_url
+            });
+        });
+      }
+      else
+      {
+        res.send(
+              {"original_url":result.original_url,
+               "short_url":result.short_url
+              });
+      }
+    });
+  //   var shortenUrl = new Url({
+  //     original_url: req.body['url'],
+  //     short_url: findLastShortUrl()
+  //   });  
+  //   shortenUrl.save();
+  //   res.json(shortenUrl);
+  console.log("valid url");
   }
   else{
-    res.send('unknown userId');
+    res.json(errorObj);
   }
-  if(from || to){
-    if(from){
-      console.log("from: ",from);
-      dateQuery["$gte"] = new Date(from);
-    }
-    if(to){
-      console.log("to: ",to);
-      dateQuery["$lte"] = new Date(to);
-    }
-    query["date"] = dateQuery;
-  }
-  console.log("datequery: "+dateQuery);
-  User.findOne({id: userId},(error, user)=>{
-    if(user == null){
-      res.send('unknown userId');
-    }
-    else{
-      var response = {};
-      response['_id']=user.id
-      response['username']=user.name
-      if(from){
-        response['from'] = from;
-      }
-      if(to){
-        response['to'] = to;
-      }
-      //res.json(response);
-      var exercises = Exercise.find(query).limit(limit);
-      exercises.exec((err,data)=>{
-        response['count']=data.length;
-        response['logs'] = data.map(ex =>{
-          return ({
-            description: ex.description,
-            duration: ex.duration,
-            date: ex.date
-          });
-        });
-        res.json(response);
-      });
-    }
-    // var exercises = Exercise.find(query).limit(limit);
-    // exercises.exec((err,data)=>{
-    // res.json({err:err,data:data});
-  });    
 });
 
-// Not found middleware
-app.use((req, res, next) => {
-  return next({status: 404, message: 'not found'})
+app.listen(port, function () {
+  console.log('Node.js listening ...');
 });
-
-
-
-// Error Handling middleware
-app.use((err, req, res, next) => {
-  let errCode, errMessage
-
-  if (err.errors) {
-    // mongoose validation error
-    errCode = 400 // bad request
-    const keys = Object.keys(err.errors)
-    // report the first validation error
-    errMessage = err.errors[keys[0]].message
-  } else {
-    // generic or custom error
-    errCode = err.status || 500
-    errMessage = err.message || 'Internal Server Error'
-  }
-  res.status(errCode).type('txt')
-    .send(errMessage)
-})
-
-const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
